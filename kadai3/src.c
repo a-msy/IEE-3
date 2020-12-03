08804c8eda1e41c920983c6337f7d29cc2a0f4d7
09430509 <psc06fz8@s.okayama-u.ac.jp>
2020-12-03 12:38:35 +0900

    kadai3

---
+++ kadai3/src.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <fcntl.h>
+#define SIZE 255
+// Use:: $./a.out | ../kadai1/a.out ../kadai1/output/1.txt
+// Use:: $../kadai2/a.out ../kadai2/input/1.txt | ./a.out
+
+int main(int argc, char *argv[]){
+    char buf[SIZE];
+
+    //file read
+    int fr = read(0, buf, SIZE);
+    if(fr == -1){
+        printf("E_READ\n");
+    }
+
+    //print
+    printf("%s\n",buf);
+
+    return 0;
+}
